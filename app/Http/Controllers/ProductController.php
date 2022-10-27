ce61a1f0c280f8ad5a2f6737fd6556efa3d9cd41
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-10-27 12:18:53 +0330

    added store product route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function store (Request $request)
+    {
+        $data = $request->validate([
+            'title' => ['required', 'string', 'max:150'],
+            'description' => ['nullable', 'string', 'max:5000']
+        ]);
+
+        $product = Product::create($data);
+
+        return response()->json($product);
+    }
 }
