9b64e1ad6c4391720d4716f7f8845209053955b4
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-03 12:16:44 +0330

    added AdminModel

---
+++ app/Models/AdminModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name',
+        'password'
+    ];
+}
